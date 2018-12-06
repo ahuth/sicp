b83dc942e29e4335a4d0d5c98bab44130d6ef9ec
Andrew Huth <andrew@huth.me>
2018-12-06 08:35:42 -0800

    Add exercise 1.5

--- src/chapter1.js
+++ src/chapter1.js
@@ -2,6 +2,13 @@ export function sumTwoLargestSquares(a, b, c) {
   return twoLargest(a, b, c).map(square).reduce(add);
 }
 
+export function sqrt(x, guess = 1, prev = 0) {
+  if (goodEnough(guess, prev)) {
+    return guess;
+  }
+  return sqrt(x, improveGuess(x, guess), guess);
+}
+
 function square(a) {
   return a * a;
 }
@@ -20,4 +27,16 @@ function twoLargest(a, b, c) {
   if (a > b && c > b) {
     return [a, c];
   }
+}
+
+function goodEnough(guess, prev) {
+  return Math.abs(guess - prev) < guess / 1000;
+}
+
+function improveGuess(x, guess) {
+  return average(guess, x / guess);
+}
+
+function average(a, b) {
+  return (a + b) / 2;
 }
--- src/chapter1.spec.js
+++ src/chapter1.spec.js
@@ -1,5 +1,9 @@
-import { sumTwoLargestSquares } from './chapter1';
+import { sumTwoLargestSquares, sqrt } from './chapter1';
 
 test('exercise 1.2', () => {
   expect(sumTwoLargestSquares(1, 2, 3)).toEqual(13);
+});
+
+test('exercise 1.5', () => {
+  expect(sqrt(137)).toBeCloseTo(11.7046999177, 8);
 });
