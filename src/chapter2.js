0619e76937ed6b76f99d1dd3e844b59d4c84ed13
Andrew Huth <andrew@huth.me>
2018-12-24 16:46:47 -0500

    Add initial implementation of exercise 2.1
    Doesn't handle negatives quite right.

--- src/chapter2.js
+++ src/chapter2.js
@@ -1,3 +1,5 @@
+import { gcd } from './chapter1';
+
 export function cons(a, b) {
   return f => f(a, b);
 }
@@ -8,4 +10,13 @@ export function car(p) {
 
 export function cdr(p) {
   return p((a, b) => b);
+}
+
+export function makeRat(numerator, denominator) {
+  const greatestCommonDenom = gcd(numerator, denominator);
+  return cons(numerator / greatestCommonDenom, denominator / greatestCommonDenom);
+}
+
+export function printRat(rat) {
+  return `${car(rat)}/${cdr(rat)}`;
 }
--- src/chapter2.spec.js
+++ src/chapter2.spec.js
@@ -1,7 +1,26 @@
-import { cons, car, cdr } from './chapter2';
+import {
+  cons,
+  car,
+  cdr,
+  makeRat,
+  addRat,
+  printRat,
+} from './chapter2';
 
 test('pairs', () => {
   const pair = cons(2, 4);
   expect(car(pair)).toEqual(2);
   expect(cdr(pair)).toEqual(4);
+});
+
+test('exercise 2.1', () => {
+  const rat1 = makeRat(2, 3);
+  const rat2 = makeRat(6, 8);
+  const rat3 = makeRat(5, -15);
+  expect(printRat(rat1)).toEqual('2/3');
+  expect(printRat(rat2)).toEqual('3/4');
+  expect(printRat(rat3)).toEqual('-1/3');
+
+  const added = addRat(rat1, rat2);
+  expect(printRat(added)).toEqual('17/12');
 });
