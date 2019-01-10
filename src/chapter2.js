bc56dc6f6f5c196b8950b7211286f444a36a5c33
Andrew Huth <ahuth@mavenlink.com>
2019-01-10 13:31:53 -0800

    Add exercise 2.37
    In the book everything is kind of imcomplete. It does not really go into "intersectionSet" and "unionSet", nor easily creating a balanced tree in the first place.
    
    Because of this I have:
    - Manually constructed balanced trees using "makeTree" in the tests
    - Commented out, but left in, the old set functions that worked on an ordered list

--- src/chapter2.js
+++ src/chapter2.js
@@ -301,68 +301,81 @@ function isNumber(x) {
 
 export function isElementOfSet(set, x) {
   if (isEmpty(set)) { return false; }
-  if (x === car(set)) { return true; }
-  if (x < car(set)) { return false; }
-  return isElementOfSet(cdr(set), x);
+  if (x === entry(set)) { return true; }
+  if (x < entry(set)) { return isElementOfSet(leftBranch(set), x); }
+  return isElementOfSet(rightBranch(set), x);
 }
 
 export function adjoinSet(set, x) {
-  if (isEmpty(set)) { return list(x); }
+  if (isEmpty(set)) { return makeTree(x, EMPTY_LIST, EMPTY_LIST); }
+  if (x === entry(set)) { return set; }
 
-  const head = car(set);
+  if (x < entry(set)) {
+    return makeTree(
+      entry(set),
+      adjoinSet(leftBranch(set), x),
+      rightBranch(set),
+    );
+  }
 
-  if (x === head) { return set; }
-  if (x < head) { return cons(x, set); }
-
-  return cons(
-    car(set),
-    adjoinSet(cdr(set), x),
+  return makeTree(
+    entry(set),
+    leftBranch(set),
+    adjoinSet(rightBranch(set), x),
   );
 }
 
-export function intersectionSet(a, b) {
-  if (isEmpty(a) || isEmpty(b)) { return EMPTY_LIST; }
+// export function intersectionSet(a, b) {
+//   if (isEmpty(a) || isEmpty(b)) { return EMPTY_LIST; }
 
-  const headA = car(a);
-  const headB = car(b);
+//   const headA = car(a);
+//   const headB = car(b);
 
-  if (headA === headB) {
-    return cons(
-      headA,
-      intersectionSet(cdr(a), cdr(b)),
-    );
-  }
+//   if (headA === headB) {
+//     return cons(
+//       headA,
+//       intersectionSet(cdr(a), cdr(b)),
+//     );
+//   }
 
-  if (headA < headB) {
-    return intersectionSet(cdr(a), b);
-  }
+//   if (headA < headB) {
+//     return intersectionSet(cdr(a), b);
+//   }
 
-  return intersectionSet(a, cdr(b));
-}
+//   return intersectionSet(a, cdr(b));
+// }
 
-export function unionSet(a, b) {
-  if (isEmpty(a)) { return b; }
-  if (isEmpty(b)) { return a; }
+// export function unionSet(a, b) {
+//   if (isEmpty(a)) { return b; }
+//   if (isEmpty(b)) { return a; }
 
-  const headA = car(a);
-  const headB = car(b);
+//   const headA = car(a);
+//   const headB = car(b);
 
-  if (headA === headB) {
-    return cons(
-      headA,
-      unionSet(cdr(a), cdr(b)),
-    );
-  }
+//   if (headA === headB) {
+//     return cons(
+//       headA,
+//       unionSet(cdr(a), cdr(b)),
+//     );
+//   }
 
-  if (headA < headB) {
-    return cons(
-      headA,
-      unionSet(cdr(a), b),
-    );
-  }
+//   if (headA < headB) {
+//     return cons(
+//       headA,
+//       unionSet(cdr(a), b),
+//     );
+//   }
 
-  return cons(
-    headB,
-    unionSet(a, cdr(b)),
-  );
+//   return cons(
+//     headB,
+//     unionSet(a, cdr(b)),
+//   );
+// }
+
+const entry = car;
+const leftBranch = cadr;
+const rightBranch = caddr;
+
+export function makeTree(entry, left, right) {
+  return list(entry, left || EMPTY_LIST, right || EMPTY_LIST);
 }
--- src/chapter2.spec.js
+++ src/chapter2.spec.js
@@ -6,7 +6,7 @@ import {
   makeInterval, upperBound, lowerBound, intAdd, intMul, intDiv, intSub, intWidth, makeIntervalPercent, intCenter, intPercent,
   list, nth, last, reverse, toString, squareList, mapCar, append, isAtom, isEmpty, deepReverse, fringe, equal,
   makeSum, makeProduct, derive,
-  unionSet, intersectionSet, adjoinSet, isElementOfSet,
+  makeTree, adjoinSet,
 } from './chapter2';
 
 test('pairs', () => {
@@ -232,23 +232,42 @@ test('symbolic differentiation', () => {
   expect(toString(derive(foo, 'c'))).toEqual(1);
 });
 
-test('exercises 2.33, 2.35, and 2.36', () => {
-  const odds = list(1, 3, 5, 7);
-  const evens = list(2, 4, 6, 8);
-  const primes = list(2, 3, 5, 7);
+test('exercises 2.33, 2.35, 2.36, and 2.37', () => {
+  // const odds = list(1, 3, 5, 7);
+  const odds = makeTree(
+    3,
+    makeTree(1),
+    makeTree(
+      5,
+      null,
+      makeTree(7),
+    ),
+  );
 
-  const otherSet = adjoinSet(evens, 666);
-  expect(toString(otherSet)).toEqual('(2 4 6 8 666)');
+  // const evens = list(2, 4, 6, 8);
+  // const primes = list(2, 3, 5, 7);
 
-  expect(isElementOfSet(odds, 2)).toEqual(false);
-  expect(isElementOfSet(evens, 2)).toEqual(true);
-  expect(isElementOfSet(primes, 2)).toEqual(true);
+  // const otherSet = adjoinSet(evens, 666);
+  // expect(toString(otherSet)).toEqual('(2 4 6 8 666)');
 
-  expect(toString(intersectionSet(odds, evens))).toEqual('()');
-  expect(toString(intersectionSet(odds, primes))).toEqual('(3 5 7)');
-  expect(toString(intersectionSet(evens, primes))).toEqual('(2)');
+  let otherSet = adjoinSet(odds, 666);
+  expect(toString(otherSet)).toEqual('(3 (1 () ()) (5 () (7 () (666 () ()))))');
 
-  expect(toString(unionSet(odds, evens))).toEqual('(1 2 3 4 5 6 7 8)');
-  expect(toString(unionSet(odds, primes))).toEqual('(1 2 3 5 7)');
-  expect(toString(unionSet(evens, primes))).toEqual('(2 3 4 5 6 7 8)');
+  otherSet = adjoinSet(otherSet, 6);
+  expect(toString(otherSet)).toEqual('(3 (1 () ()) (5 () (7 (6 () ()) (666 () ()))))');
+
+  otherSet = adjoinSet(otherSet, 13);
+  expect(toString(otherSet)).toEqual('(3 (1 () ()) (5 () (7 (6 () ()) (666 (13 () ()) ()))))');
+
+  // expect(isElementOfSet(odds, 2)).toEqual(false);
+  // expect(isElementOfSet(evens, 2)).toEqual(true);
+  // expect(isElementOfSet(primes, 2)).toEqual(true);
+
+  // expect(toString(intersectionSet(odds, evens))).toEqual('()');
+  // expect(toString(intersectionSet(odds, primes))).toEqual('(3 5 7)');
+  // expect(toString(intersectionSet(evens, primes))).toEqual('(2)');
+
+  // expect(toString(unionSet(odds, evens))).toEqual('(1 2 3 4 5 6 7 8)');
+  // expect(toString(unionSet(odds, primes))).toEqual('(1 2 3 5 7)');
+  // expect(toString(unionSet(evens, primes))).toEqual('(2 3 4 5 6 7 8)');
 });
