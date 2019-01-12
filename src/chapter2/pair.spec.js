474c250bae8dd00c280a9bd3b9aef4878e8086fa
Andrew Huth <ahuth@mavenlink.com>
2019-01-12 07:56:13 -0800

    Backfill missing "pair" tests

--- src/chapter2/pair.spec.js
+++ src/chapter2/pair.spec.js
@@ -1,7 +1,13 @@
-import { cons, car, cdr } from './pair';
+import { cons, car, cdr, cadr, caddr } from './pair';
 
 test('pairs', () => {
   const pair = cons(2, 4);
   expect(car(pair)).toEqual(2);
   expect(cdr(pair)).toEqual(4);
+});
+
+test('cadr and caddr', () => {
+  const pair = cons(1, cons(2, cons(3, cons(4, null))));
+  expect(cadr(pair)).toEqual(2);
+  expect(caddr(pair)).toEqual(3);
 });
