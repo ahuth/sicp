80c7b0ea72649f3cc599c889720b2e44a69b3c9e
Andrew Huth <andrew@huth.me>
2019-01-16 08:37:20 -0800

    Add more complex polynomial arithmetic example

--- src/chapter2/polynomial-arithmetic.spec.js
+++ src/chapter2/polynomial-arithmetic.spec.js
@@ -5,9 +5,12 @@ import { makePolynomial, makeTerm, tagPolynomial } from './polynomial-arithmetic
 test('adding', () => {
   const p1 = tagPolynomial(makePolynomial('x', list(makeTerm(2, 1)))); // 1 * x^2
 
-  const added = add(p1, p1); // x^2 + x^2
-  expect(toString(added)).toEqual('(x (2 2))');  // 2 * x^2
+  const p2 = add(p1, p1); // x^2 + x^2
+  expect(toString(p2)).toEqual('(x (2 2))');  // 2 * x^2
 
-  const multiplied = mul(p1, p1); // x^2 * x^2
-  expect(toString(multiplied)).toEqual('(x (4 1))');  // 1 * x^4
+  const p3 = mul(p1, p1); // x^2 * x^2
+  expect(toString(p3)).toEqual('(x (4 1))');  // 1 * x^4
+
+  const p4 = add(tagPolynomial(p2), tagPolynomial(p3)); // (2x^2 + x^4)
+  expect(toString(p4)).toEqual('(x (4 1) (2 2))');
 });
