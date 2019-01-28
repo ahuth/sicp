85704cba8e8639e68b966f4c3874e483c659a2e3
Andrew Huth <andrew@huth.me>
2019-01-27 20:56:32 -0800

    Add two dimensional table example

--- src/chapter3/table.spec.js
+++ src/chapter3/table.spec.js
@@ -7,3 +7,10 @@ test('tables', () => {
   insert(t, 'hello', 'world');
   expect(lookup(t, 'hello')).toEqual('world');
 });
+
+test('2-dimensional tables', () => {
+  const t = makeTable();
+  insert(t, 'abc', makeTable());
+  insert(lookup(t, 'abc'), '123', 'hello there');
+  expect(lookup(lookup(t, 'abc'), '123')).toEqual('hello there');
+});
